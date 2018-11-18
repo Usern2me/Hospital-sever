f789fd64f772493bcb2af7c60fed125a7959b717
cocoroise <593996605@qq.com>
2018-11-18 23:05:25 +0800

    init

---
+++ src/app.js
@@ -0,0 +1,39 @@
+import Koa from 'koa'
+import bodyParser from 'koa-bodyparser'
+import logger from 'koa-logger'
+import session from 'koa-session'
+import convert from 'koa-convert'
+import koaStatic from 'koa-static'
+import cors from 'koa-cors'
+import path from 'path'
+import router from './router'
+import mongoose from 'mongoose'
+
+import config from './config'
+import respondFormatter from './middlewares/respondFormatter'
+
+const app = new Koa()
+
+mongoose.Promise = global.Promise
+mongoose.connect(config.mongoConfig.url, {
+  useMongoClient: true
+})
+
+app.keys = ['ocean-domon']
+app.context.config = config
+
+app.use(cors({
+  maxAge: 7 * 24 * 60 * 60,
+  credentials: true,
+  methods: 'GET, HEAD, OPTIONS, PUT, POST, PATCH, DELETE',
+  headers: 'Content-Type, Accept, Authorization'
+}))
+.use(logger())
+.use(bodyParser())
+.use(convert(session(app)))
+.use(koaStatic(path.join(__dirname, '/public')))
+.use(respondFormatter('^/api'))   // 仅格式化api开头的地址输出
+.use(router.routes())
+.use(router.allowedMethods())
+
+export default app
